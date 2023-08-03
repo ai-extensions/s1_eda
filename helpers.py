08e35bd8265edb322d7b1db360db41c47f26fafe
simonevaccari <simone.vaccari@terradue.com>
2023-08-03 13:50:51 +0000

    added stackstac item for one s2item

--- helpers.py
+++ helpers.py
@@ -30,50 +30,6 @@ def find_centroid(bboxes):
     return centroid_x, centroid_y
 
 
-# def showMap_BBOX(items):
-    
-#     bbox_list = []
-#     for item in items: 
-#         # print(f'Get bbox of {item.id} (datetime={item.properties["datetime"]})')
-#         bbox_list.append(item.bbox)
-#     # print(bbox_list)
-    
-#     centroid_x, centroid_y = find_centroid(bbox_list)
-
-#     # Create a folium map centered on a specific location (e.g., California)
-#     map_center = [centroid_y, centroid_x]
-#     mymap = folium.Map(location=map_center, zoom_start=7)
-
-#     # Create separate FeatureGroups for each bounding box layer
-#     for i, bbox in enumerate(bbox_list):
-#         feature_group = folium.FeatureGroup(name=f'BBOX {i+1}')
-#         mymap.add_child(feature_group)
-
-#         # Extract the coordinates
-#         min_lon, min_lat, max_lon, max_lat = bbox
-
-#         # Calculate the center of the bounding box
-#         center_lat = (min_lat + max_lat) / 2
-#         center_lon = (min_lon + max_lon) / 2
-
-#         # Create a rectangle using the bounding box coordinates
-#         rect = folium.Rectangle(
-#             bounds=[(min_lat, min_lon), (max_lat, max_lon)],
-#             color='blue',
-#             fill=True,
-#             fill_color='blue',
-#             fill_opacity=0.3,
-#             tooltip=f'BBOX {i+1}'
-#         )
-
-#         # Add the rectangle to the feature group
-#         rect.add_to(feature_group)
-
-#     # Add LayerControl to the map
-#     folium.LayerControl().add_to(mymap)
-
-#     return mymap
-
 def showMap_BBOX(items):
     
     bbox_list = {}
